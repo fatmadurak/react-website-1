8d8e5a58318b703ae404df05432f71fdc30aef98
fatmadurak <fatmabadillidurak@gmail.com>
2022-12-26 12:59:10 +0300

    img-commited

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    
+    <nav className='navbar'>
+        
+        <div className='navbar-container'>
+           
+            <link to="/" className='navbar-logo'> TRVL</link>
+            
+        </div>
+        
+    </nav>
+    
+    
+    </>
+  )
+}
+
+export default Navbar;
