ee324b47fef99bf0592ba1e4f2b1cac7f1f8b94b
fatmadurak <fatmabadillidurak@gmail.com>
2022-12-29 14:46:23 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Product from './components/pages/Product';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
        <Navbar/>
        <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Product/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
         </Routes>
      </Router>
    
---
+++ src/components/pages/Services.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import "../../App.css";
+
+function Services() {
+  return (
+   <h1 className='services'> SERVİCES</h1>
+  )
+}
+
+export default Services
